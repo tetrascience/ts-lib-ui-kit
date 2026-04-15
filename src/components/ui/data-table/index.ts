660391276203aa09f0856aab9a2faffc42c96d6e
Oseer Williams <265368733+owilliams-tetrascience@users.noreply.github.com>
2026-04-15 11:45:33 -0400

    chore(): update data table exports

--- src/components/ui/data-table/index.ts
+++ src/components/ui/data-table/index.ts
@@ -1,8 +1,3 @@
-export { DataTable, TableToolbar, useDataTable } from "./data-table"
-export type { DataTableProps } from "./data-table"
-
-export { DataTableColumnToggle } from "./data-table-column-toggle"
-export type { DataTableColumnToggleProps } from "./data-table-column-toggle"
-
-export { DataTablePagination } from "./data-table-pagination"
-export type { DataTablePaginationProps } from "./data-table-pagination"
+export * from "./data-table"
+export * from "./data-table-column-toggle"
+export * from "./data-table-pagination"
